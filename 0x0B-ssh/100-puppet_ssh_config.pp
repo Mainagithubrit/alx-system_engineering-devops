1460d1e87372c33bfb468f44a5493582b5f630d0
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-02-25 23:00:22 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@
 # to connect to a server without typing a password
 
 file_line { 'Turn off passwd auth':
-	ensure  => present,
-	line	=> '	PasswordAuthentication no''
-	path	=> '/etc/ssh/ssh_config',
+  ensure => present,
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
 }
 
-file_line { 'Declare identity file'
-	ensure	=> present'
-	line	=> '	IdentityFile ~/.ssh/school',
-	path	=> '/etc/ssh/ssh_config',
+file_line { 'Declare identity file':
+  ensure => present,
+  line   => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
 }
