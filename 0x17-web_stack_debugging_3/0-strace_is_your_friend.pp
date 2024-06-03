ca88e9882c9b94953aa217171fd69f5354b5e09c
Maina Njoroge <njorogefrancismaina@gmail.com>
2024-06-03 20:33:37 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Creates a manifest that fixes phpp extension
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin/', 'usr/local/bin/'],
-}
