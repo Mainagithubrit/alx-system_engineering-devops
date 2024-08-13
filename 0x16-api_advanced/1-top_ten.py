4c95ee718a2ea81a3a3b460b86cc571b6a26c437
Mainagithubrit <njorogrfrancismaina@gmail.com>
2024-08-13 21:15:23 +0300

    ALX

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API
+and prints the titles of the first 10 hot posts listed for a given subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    # Construct the URL for the subreddit's hot posts in JSON format
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    # Define headers for the HTTP request, including User-Agent
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 404:
+        print("None")
+        return
+
+    results = response.json().get("data")
+
+    [print(c.get("data").get("title")) for c in results.get("children")]
